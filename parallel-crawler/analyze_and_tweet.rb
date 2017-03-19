4f1309013ed7c15249da54a20dc06782b10e9cd8
Hiroaki Ninomiya <me@treby.info>
2017-03-19 16:45:32 +0900

    update tweet

--- parallel-crawler/analyze_and_tweet.rb
+++ parallel-crawler/analyze_and_tweet.rb
@@ -25,18 +25,30 @@ end
 idol_points = CSV.open(Dir.glob(File.dirname(__FILE__) + '/outputs/*.csv').last).to_a.transpose.map{ |arr| arr.shift; arr.map(&:to_i) }[1..-1]
 idol_rankings = (14..50).map{ |n| Rubimas.find(n) }.zip(idol_points).map(&:flatten)
 
+source = ""
+
 idol_rankings.each do |idol_rank|
   idol = idol_rank.first
-  puts "＜#{idol.name}＞"
-  puts "1位: #{readable_unit(idol_rank[1])}"
-  puts "10位: #{readable_unit(idol_rank[10])}"
-  puts "100位: #{readable_unit(idol_rank[100])}"
-  puts "200位: #{readable_unit(idol_rank[200])}"
-  puts "BMD(254位): #{readable_unit(idol_rank[254])}"
-  puts "300位: #{readable_unit(idol_rank[300])}"
-  puts '====='
-  puts "5位先(#{reward + 5}位)との差: #{readable_unit(idol_rank[reward] - idol_rank[reward + 5])}"
-  puts "10位先(#{reward + 10}位)との差: #{readable_unit(idol_rank[reward] - idol_rank[reward + 10])}"
-  puts "20位先(#{reward + 20}位)との差: #{readable_unit(idol_rank[reward] - idol_rank[reward + 20])}"
+  source += "＜#{idol.name}＞\n"
+  source += "1位: #{readable_unit(idol_rank[1])}\n"
+  source += "10位: #{readable_unit(idol_rank[10])}\n"
+  source += "100位: #{readable_unit(idol_rank[100])}\n"
+  source += "200位: #{readable_unit(idol_rank[200])}\n"
+  source += "BMD(254位): #{readable_unit(idol_rank[254])}\n"
+  source += "300位: #{readable_unit(idol_rank[300])}\n"
+  source += "=====\n"
+  source += "5位先(#{reward + 5}位)との差: #{readable_unit(idol_rank[reward] - idol_rank[reward + 5])}\n"
+  source += "10位先(#{reward + 10}位)との差: #{readable_unit(idol_rank[reward] - idol_rank[reward + 10])}\n"
+  source += "20位先(#{reward + 20}位)との差: #{readable_unit(idol_rank[reward] - idol_rank[reward + 20])}\n"
 end
 
+puts source
+if prev_tweet
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+    config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+  end
+  client.update "ランナー分布を更新しました。\nhttp://mlborder.com/misc/runners?event=tys", in_reply_to_status_id: prev_tweet
+end
--- parallel-crawler/bot_endpoint.rb
+++ parallel-crawler/bot_endpoint.rb
@@ -1,5 +1,11 @@
+require 'optparse'
+
+params = ARGV.getopts('', 'reward_rank:', 'prev_tweet_id:')
+reward_rank = params['reward_rank'] ? params['reward_rank'].to_i : nil
+prev_tweet_id = params['prev_tweet_id']
+
 if system('bundle exec ruby get_ranking_all.rb -e 350; bundle exec ruby make_csv.rb')
-  system('bundle exec ruby upload_csv_to_s3.rb')
+  system('bundle exec ruby upload_csv_to_s3.rb') && system("bundle exec ruby analyze_and_tweet.rb --reward #{reward_rank} --prev_tweet #{prev_tweet_id}")
 else
-  puts '=== Error Occurred! ==='
+  raise '=== Error Occurred! ==='
 end
