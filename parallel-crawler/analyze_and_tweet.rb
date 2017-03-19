fcc93852e1d1cfb724233a80ce75617cc769f4f4
Hiroaki Ninomiya <me@treby.info>
2017-03-19 22:54:14 +0900

    feat: update analyze_tweet

--- parallel-crawler/analyze_and_tweet.rb
+++ parallel-crawler/analyze_and_tweet.rb
@@ -2,6 +2,7 @@ require 'twitter'
 require 'rubimas'
 require 'optparse'
 require 'csv'
+require 'pp'
 
 params = ARGV.getopts('', 'reward:', 'prev_tweet:')
 reward = params['reward'] ? params['reward'].to_i : nil
@@ -22,27 +23,40 @@ def readable_unit(number)
   after_digit <= 0 ? "#{num.to_i}#{unit}" : "#{format("%.#{after_digit}f", num)}#{unit}"
 end
 
+def ajust_space(text, length)
+  text_length = text.chars.map { |c| c.ascii_only? ? 1 : 2 }.sum
+  ws_count = length - text_length
+  "#{text + (' ' * ws_count)}"
+end
+
 idol_points = CSV.open(Dir.glob(File.dirname(__FILE__) + '/outputs/*.csv').last).to_a.transpose.map{ |arr| arr.shift; arr.map(&:to_i) }[1..-1]
 idol_rankings = (14..50).map{ |n| Rubimas.find(n) }.zip(idol_points).map(&:flatten)
 
-source = ""
-
-idol_rankings.each do |idol_rank|
+idol_summaries = idol_rankings.map do |idol_rank|
+  summary = []
   idol = idol_rank.first
-  source += "＜#{idol.name}＞\n"
-  source += "1位: #{readable_unit(idol_rank[1])}\n"
-  source += "10位: #{readable_unit(idol_rank[10])}\n"
-  source += "100位: #{readable_unit(idol_rank[100])}\n"
-  source += "200位: #{readable_unit(idol_rank[200])}\n"
-  source += "BMD(254位): #{readable_unit(idol_rank[254])}\n"
-  source += "300位: #{readable_unit(idol_rank[300])}\n"
-  source += "=====\n"
-  source += "5位先(#{reward + 5}位)との差: #{readable_unit(idol_rank[reward] - idol_rank[reward + 5])}\n"
-  source += "10位先(#{reward + 10}位)との差: #{readable_unit(idol_rank[reward] - idol_rank[reward + 10])}\n"
-  source += "20位先(#{reward + 20}位)との差: #{readable_unit(idol_rank[reward] - idol_rank[reward + 20])}\n"
+  summary << "＜#{idol.name}＞"
+  summary << "1位: #{readable_unit(idol_rank[1])}"
+  summary << "100位: #{readable_unit(idol_rank[100])}"
+  summary << "200位: #{readable_unit(idol_rank[200])}"
+  summary << "BMD(254位): #{readable_unit(idol_rank[254])}"
+  summary << "300位: #{readable_unit(idol_rank[300])}"
+  summary << ""
+  summary << "+5(#{reward + 5}位)との差: #{readable_unit(idol_rank[reward] - idol_rank[reward + 5])}"
+  summary << "+10(#{reward + 10}位)との差: #{readable_unit(idol_rank[reward] - idol_rank[reward + 10])}"
+  summary << "+20(#{reward + 20}位)との差: #{readable_unit(idol_rank[reward] - idol_rank[reward + 20])}"
+  summary << ""
+  summary << ""
 end
 
-puts source
+lines = idol_summaries.each_slice(4).map do |line_idols|
+  line_idols.transpose.map do |elm|
+    "#{elm.map { |li| ajust_space(li, 30) }.join}"
+  end
+end
+
+`convert -background white -fill black -font migu-1m-regular.ttf -pointsize 18 -interline-spacing 4 -kerning 0.5 label:'#{lines.join("\n")}' outputs/2017_tys_runners.png`
+
 if prev_tweet
   client = Twitter::REST::Client.new do |config|
     config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
